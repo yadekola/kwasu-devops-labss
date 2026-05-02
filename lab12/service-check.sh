bfcecffac55f5f3a8f93b2f90f06c74f181edb2c
yadekola <yakubadekolaojo059@gmail.com>
2026-05-02 21:22:33 +0000

    New Update Lab 12 Linux Service Management

--- lab12/service-check.sh
+++ lab12/service-check.sh
@@ -10,7 +10,3 @@ systemctl is-enabled httpd
 systemctl status sshd --no-pager
 systemctl cat httpd
 echo "Done"
-chmod +x service-check.sh
-./service-check.sh
-./service-check.sh > service-check.log
-cat service-check.log
